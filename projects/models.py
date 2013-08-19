b973cc807aa66efb4dc56e111306963e6da44e68
garretraziel <boloomka@gmail.com>
2013-08-19 21:42:55 +0200

    moved base template for blog, started templates for projects

--- projects/models.py
+++ projects/models.py
@@ -1,27 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 from django.db import models
 
 
 class ProjectTag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField("jméno", max_length=255)
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "tag"
+        verbose_name_plural = "tagy"
 
 
 class LanguageTag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField("jméno", max_length=255)
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "jazyk"
+        verbose_name_plural = "jazyky"
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField("jméno", max_length=255)
     url = models.URLField("project url")
-    summary = models.TextField()
+    summary = models.TextField("souhrn")
 
-    tags = models.ManyToManyField(ProjectTag)
-    languages = models.ManyToManyField(LanguageTag)
+    tags = models.ManyToManyField(ProjectTag, verbose_name="tagy")
+    languages = models.ManyToManyField(LanguageTag, verbose_name="jazyky")
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "projekt"
+        verbose_name_plural = "projekty"
--- projects/views.py
+++ projects/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from projects.models import LanguageTag, ProjectTag, Project
+
+
+class ProjectListView(ListView):
+    context_object_name = "projects"
+    queryset = Project.objects.order_by("name")
+    template_name = "projects/index.html"
