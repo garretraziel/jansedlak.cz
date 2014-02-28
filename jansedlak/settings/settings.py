b06f04bc2fe0955b83d4474d4431e5c8c8b8c893
Garret Raziel <boloomka@gmail.com>
2014-02-28 14:25:37 +0100

    few changes in settings

--- jansedlak/settings/settings.py
+++ jansedlak/settings/settings.py
@@ -4,7 +4,7 @@
 import os
 BASE_DIR = os.path.join(os.path.dirname(__file__), "..", "..")
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
@@ -36,7 +36,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/var/www/media/'
+MEDIA_ROOT = '/mnt/media/'
 #MEDIA_ROOT = '/tmp/www/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
@@ -48,7 +48,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = '/mnt/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
